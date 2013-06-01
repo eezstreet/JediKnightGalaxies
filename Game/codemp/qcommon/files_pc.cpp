98dd8e11ef5b411b02b7dc12ab49e0fa7952c0cb
eezstreet <eezstreet@live.com>
2013-06-01 00:02:13 -0400

    I don't give two shits about not-precached sounds, bro.

--- Game/codemp/qcommon/files_pc.cpp
+++ Game/codemp/qcommon/files_pc.cpp
@@ -852,9 +852,12 @@ int FS_FOpenFileRead( const char *filename, fileHandle_t *file, qboolean uniqueF
 	#ifndef DEDICATED
 	#ifndef FINAL_BUILD
 						// Check for unprecached files when in game but not in the menus
-						if((cls.state == CA_ACTIVE) && !(Key_GetCatcher( ) & KEYCATCH_UI))
+						if( com_developer->integer )
 						{
-							Com_Printf(S_COLOR_YELLOW "WARNING: File %s not precached\n", filename);
+							if((cls.state == CA_ACTIVE) && !(Key_GetCatcher( ) & KEYCATCH_UI))
+							{
+								Com_Printf(S_COLOR_YELLOW "WARNING: File %s not precached\n", filename);
+							}
 						}
 	#endif
 	#endif // DEDICATED
